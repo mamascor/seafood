3fe94c48336612a45a783a380ab67d3e2e7b232c
Marco Mascorro <marco.mascorroa@icloud.com>
2023-08-13 16:29:49 -0500

    hot dog not hotdog

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -6,14 +6,160 @@
 //
 
 import UIKit
+import AVFoundation
+import CoreML
+import CreateML
 
 class ViewController: UIViewController {
+    
+    var captureSession: AVCaptureSession!
+    var stillImageOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .medium
+
+        guard let backCamera = AVCaptureDevice.default(for: .video) else { return }
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera)
+            stillImageOutput = AVCapturePhotoOutput()
+
+            if captureSession.canAddInput(input) && captureSession.canAddOutput(stillImageOutput) {
+                captureSession.addInput(input)
+                captureSession.addOutput(stillImageOutput)
+                setupLivePreview()
+            }
+        }
+        catch let error {
+            print("Error: \(error.localizedDescription)")
+        }
+    }
+
+    func setupLivePreview() {
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = .resizeAspect
+        previewLayer.connection?.videoOrientation = .portrait
+        view.layer.addSublayer(previewLayer)
+
+        let buttonContainerView = UIView()
+        view.addSubview(buttonContainerView)
+        view.bringSubviewToFront(buttonContainerView)
+
+        let captureButton = UIButton()
+        captureButton.backgroundColor = .white
+        captureButton.layer.cornerRadius = 30
+        captureButton.translatesAutoresizingMaskIntoConstraints = false // Enable Auto Layout
+        captureButton.addTarget(self, action: #selector(captureImage), for: .touchUpInside)
+
+        buttonContainerView.addSubview(captureButton)
+
+        // Set up Auto Layout constraints for the button
+        NSLayoutConstraint.activate([
+            captureButton.centerXAnchor.constraint(equalTo: buttonContainerView.centerXAnchor),
+            captureButton.bottomAnchor.constraint(equalTo: buttonContainerView.bottomAnchor, constant: -20),
+            captureButton.widthAnchor.constraint(equalToConstant: 60),
+            captureButton.heightAnchor.constraint(equalToConstant: 60)
+        ])
+
+        buttonContainerView.frame = view.bounds
+
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            self?.captureSession.startRunning()
+            DispatchQueue.main.async { [weak self] in
+                self?.previewLayer.frame = self?.view.bounds ?? CGRect.zero
+            }
+        }
     }
 
 
+
+    @objc func captureImage() {
+        print("papa")
+        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
+        stillImageOutput.capturePhoto(with: settings, delegate: self)
+        
+        print("afr")
+    }
+
+    // Make sure to stop the session on exit
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.captureSession.stopRunning()
+    }
+    
+    func buffer(from image: UIImage) -> CVPixelBuffer? {
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer: CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(image.size.width), Int(image.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard status == kCVReturnSuccess else {
+            return nil
+        }
+
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(image.size.width), height: Int(image.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+
+        context?.translateBy(x: 0, y: image.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+
+        UIGraphicsPushContext(context!)
+        image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
+        UIGraphicsPopContext()
+
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+
+        return pixelBuffer
+    }
 }
 
+
+extension ViewController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        print("1")
+        guard let imageData = photo.fileDataRepresentation() else { return }
+        print("12")
+        guard let image = UIImage(data: imageData) else { return }
+        print("123")
+        guard let resizedImage = image.resized(to: CGSize(width: 299, height: 299)) else { return }
+
+        guard let pixelBuffer = buffer(from: resizedImage) else { return }
+        print("1234")
+        let model = try! hoteval(configuration: MLModelConfiguration())
+        
+        
+        guard let prediction = try? model.prediction(image: pixelBuffer) else {
+            print("Prediction failed!")
+            return
+        }
+
+        // Use the prediction results
+        if prediction.classLabel == "hot_dog" {
+            print("Hot Dog")
+        } else {
+            print("Not hot dog")
+        }
+        
+    }
+}
+
+extension UIImage {
+    func resized(to size: CGSize) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(size, false, self.scale)
+        self.draw(in: CGRect(origin: .zero, size: size))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return resizedImage
+    }
+}
